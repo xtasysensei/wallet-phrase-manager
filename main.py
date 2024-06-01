cbf2721704ff6b0be9129a03893ecd6a421f3199
xtasysensei <maiwuyashem@gmail.com>
2024-06-01 07:57:09 +0100

    first commit

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-import json
-import os
+import sys
+
 from core.core import *
 
-def start():
-    print("Welcome to phraser")
-    wallet_name = input("Which wallet do you want to save phrases for: ")
 
+def lines():
+    print("----------------------")
+
+
+def add_wallet():
+    wallet_name = input("Which wallet do you want to save phrases for: ")
     wallet_init = WalletName(wallet_name)
     wallet_init.create_wallet()
 
@@ -23,6 +26,36 @@ def start():
 
     init_wallet = InitializeWallet()
     init_wallet.initialize_wallet(phrase_amount, wallet_name, hash_password)
+
+
+def start():
+    while True:
+        try:
+            print("Welcome to PhraSer")
+            print("What do you want to do?")
+            print(f"    [1] Add wallet")
+            print(f"    [2] Remove wallet")
+            print(f"    [3] Retrieve wallet")
+            print(f"    [q] Exit")
+            choice: str = input("option:> ")
+            if choice not in ('1', '2', '3', 'q'):
+                print("Please enter an option (1, 2, 3, q)")
+            else:
+                break
+        except ValueError:
+            print("Invalid input. Please enter a option (1, 2, 3, q).")
+
+    if choice == "1":
+        add_wallet()
+    #elif choice == "2":
+    #elif choice == "3":
+    elif choice == "q":
+        lines()
+        print("Exiting PhraSer...")
+        lines()
+        sys.exit()
+    else:
+        print("Invalid input. Please enter a option (1, 2, 3, q).")
 
 
 if __name__ == "__main__":
